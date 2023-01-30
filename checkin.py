7886d4d99a6b4e07bb8b3c64c36c544ffe65555c
Maozhuxikuawoshuai <383607590@qq.com>
2023-01-30 11:22:53 +0800

    Create checkin.py

---
+++ checkin.py
@@ -0,0 +1,16 @@
+import requests
+
+url = 'https://glados.rocks/api/user/checkin'
+data = {'token': 'glados.network'}
+headers = {
+'authorization': '65514417340718653465740470278038-1080-1920',
+'Content-type': 'application/json;charset=utf-8',
+'accept': 'application/json, text/plain, */*',
+'cookie': '_ga=GA1.2.708365971.1675044132; _gid=GA1.2.1268817973.1675044132; koa:sess=eyJ1c2VySWQiOjI2NzQ1OCwiX2V4cGlyZSI6MTcwMDk2NjMyNjM3NiwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=GOjBA2u8irXWxHqfAr1iHdbZ5hE; Cookie=enabled; Cookie.sig=lbtpENsrE0x6riM8PFTvoh9nepc',
+'origin': 'https://glados.rocks'
+}
+
+response = requests.post(url, json=data, headers=headers)
+
+print(response.status_code)
+print(response.json())
